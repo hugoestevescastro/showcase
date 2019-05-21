1660cc3e233e4bec7ac9b0dbbfe11280acd98583
Hugo Esteves de Castro <hugoestevescastro@gmail.com>
2019-05-21 15:49:43 +0100

    complete intro animation

--- src/app/central/central.component.ts
+++ src/app/central/central.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { sleep } from '../utilities';
 
 @Component({
   selector: 'app-central',
@@ -7,8 +8,13 @@ import { DeviceDetectorService } from 'ngx-device-detector';
   styleUrls: ['./central.component.sass']
 })
 export class CentralComponent implements OnInit {
-  @ViewChild('name') name: ElementRef;
+  @ViewChild('info') info: ElementRef;
+  @ViewChild('svg') svg: ElementRef;
+
   public isMobile: boolean;
+  public isAnimated: boolean = false;
+  private isAnimationComplete: boolean = false;
+
   constructor(private deviceService: DeviceDetectorService, private renderer: Renderer2) {
     this.checkDevice();
   }
@@ -16,13 +22,27 @@ export class CentralComponent implements OnInit {
   ngOnInit() {
   }
 
+  async ngAfterViewChecked() {
+    if(this.isAnimated && !this.isAnimationComplete) {
+      await this.fadeIn();
+    }
+  }
+
   checkDevice() {
     this.isMobile = this.deviceService.isMobile();
   }
 
-  fadeOut() {
-    this.renderer.setStyle(this.name.nativeElement, 'animation', 'fade-out 1s');
-    this.renderer.setStyle(this.name.nativeElement, 'opacity', '0');
+  async fadeOut() {
+    this.renderer.setStyle(this.info.nativeElement, 'animation', 'fade-out 1s');
+    this.renderer.setStyle(this.info.nativeElement, 'opacity', '0');
+    await sleep(1000);
+    this.isAnimated = true;
+  }
+  async fadeIn() {
+    this.isAnimationComplete = true;
+    this.renderer.setStyle(this.svg.nativeElement, 'animation', 'rotate 1s cubic-bezier(0.785, 0.135, 0.150, 0.860) both');
+    this.renderer.setStyle(this.svg.nativeElement, 'opacity', '1');
+    await sleep(1000);
   }
 
 }

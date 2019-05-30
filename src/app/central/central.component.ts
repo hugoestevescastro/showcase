526fa5c8fde2ca24616f600743ba56575470e997
Hugo Esteves de Castro <hugoestevescastro@gmail.com>
2019-05-30 17:20:39 +0100

    mobile animations

--- src/app/central/central.component.ts
+++ src/app/central/central.component.ts
@@ -10,6 +10,8 @@ import { sleep } from '../utilities';
 export class CentralComponent implements OnInit {
   @ViewChild('info') info: ElementRef;
   @ViewChild('svg') svg: ElementRef;
+  @ViewChild('mobileinfo_1') mobileinfo1: ElementRef;
+  @ViewChild('mobileinfo_2') mobileinfo2: ElementRef;
 
   public isMobile: boolean;
   public isAnimated: boolean = false;
@@ -26,35 +28,39 @@ export class CentralComponent implements OnInit {
     this.isMobile = this.deviceService.isMobile();
   }
   async ngAfterViewChecked() {
-    if(this.isAnimated && !this.isAnimationComplete) {
-      this.animation_intro_s2();
+    if(this.isAnimated && !this.isAnimationComplete && !this.isMobile) {
+      this.desktop_animation_intro_s2();
     }
-    else if (!this.isAnimated && !this.isAnimationComplete && this.isToGoBack) {
-      await this.animation_end_s2();
+    else if (!this.isAnimated && !this.isAnimationComplete && this.isToGoBack && !this.isMobile) {
+      await this.desktop_animation_end_s2();
     }
   }
-
-  async animation_intro_s1() {
+  async desktop_animation_intro_s1() {
     this.renderer.addClass(this.info.nativeElement, 'fade-out-animation');
     await sleep(1000);
     this.isAnimated = true;
   }
-  async animation_intro_s2() {
+  async desktop_animation_intro_s2() {
     this.isAnimationComplete = true;
     this.renderer.addClass(this.svg.nativeElement, 'cubic-animation-appear');
     await sleep(1000);
   }
-  async animation_end_s1() {
+  async desktop_animation_end_s1() {
     this.renderer.addClass(this.svg.nativeElement, 'cubic-animation-disappear');
     await sleep(1000);
     this.isAnimationComplete = false;
     this.isAnimated = false;
     this.isToGoBack = true;
   }
-  async animation_end_s2() {
+  async desktop_animation_end_s2() {
     this.renderer.addClass(this.info.nativeElement, 'fade-in-animation');
     await sleep(1000);
     this.isToGoBack = false;
   }
-
+  async mobile_animation_intro() {
+    this.renderer.addClass(this.mobileinfo1.nativeElement, 'fade-out-animation');
+    this.renderer.addClass(this.mobileinfo2.nativeElement, 'fade-out-animation');
+    this.renderer.addClass(this.svg.nativeElement, 'scale-animation');
+    this.isAnimated = true;
+  }
 }
